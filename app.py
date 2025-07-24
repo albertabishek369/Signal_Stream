ef3c96479c12d5b66c660d34c922b780529077ec
AiLearn360 <learncodevisually@gmail.com>
2025-07-24 15:16:56 +0530

    added this app = create_app() before the main in app.py

--- app.py
+++ app.py
@@ -119,6 +119,7 @@ def create_app(config_class=Config):
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
