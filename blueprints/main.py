1072a71bba402cbe9cd4839f3317a84c4d301462
AiLearn360 <learncodevisually@gmail.com>
2025-07-25 09:06:47 +0530

    payment testing

--- blueprints/main.py
+++ blueprints/main.py
@@ -194,7 +194,7 @@ def update_profile():
 def create_subscription():
     user_id = current_user.id
     plan_type = request.get_json().get('plan_type')
-    plan_ids = {'founder': 'plan_Pt9lnya6jJDPgN', 'scale': 'plan_Qwt1O8dXzFcNNF'}
+    plan_ids = {'founder': 'plan_QwswYisjnDCDY6', 'scale': 'plan_Qwt1O8dXzFcNNF'}
     
     if plan_type not in plan_ids:
         return jsonify({'error': 'Invalid plan type'}), 400
